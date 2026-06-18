ede4a49ea38cce202b971cc506da6afedbfcc6a5
andre.salaschinchilla <andre.salaschinchilla@ucr.ac.cr>
2026-06-17 18:38:49 -0600

    Data cleaning simplified, better data analysis added

--- Data/Cleaner.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-data = pd.read_csv('ParmBotQuestionnaire.csv')
-data = data.drop(columns=['Marca temporal', "Código"])
-data = data.dropna(axis=1)
-data["Caso"] = data["Caso"].replace({
-    "Caso A - ParmBot interactivo": "A",
-    "Caso B - ParmBot simple": "B"
-})
-data.to_csv('ParmBotQuestionnaire.csv', index=False)
